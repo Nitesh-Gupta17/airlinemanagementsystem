0ae5751c02cb66fcdc3fc1c873debb6e2a5634d7
Nitesh-Gupta17 <131606845+Nitesh-Gupta17@users.noreply.github.com>
2024-05-30 12:59:11 +0530

    Add files via upload

---
+++ AirlineManagementSystem/src/airlinemanagementsystem/Conn.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package airlinemanagementsystem;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "admin123");
+            s = c.createStatement();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+}
